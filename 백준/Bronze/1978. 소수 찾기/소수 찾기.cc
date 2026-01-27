a992a1d805500e672038c60079b841d3d8bee729
공지현 <83187050+bluepaper14@users.noreply.github.com>
2026-01-27 22:36:08 +0900

    Refactor prime number counting logic
    주석수정

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int T;
     int x;
-    int pls = 0; // 소수의 개수를 셀 변수
+    int pls = 0;
 
     cin >> T;
     for (int i = 0; i < T; i++) {
@@ -13,16 +13,14 @@ int main() {
 
         if (x <= 1) continue; // 1은 소수가 아님
 
-        bool isPrime = true; // 소수인지 판별할 깃발(flag)
-
+        bool isPrime = true;
         // 2부터 x-1까지 나누어본다
         for (int j = 2; j < x; j++) {
             if (x % j == 0) {
                 isPrime = false; // 나누어떨어지면 소수가 아님!
-                break;           // 더 볼 필요도 없이 탈출
+                break;          
             }
         }
-
         // 루프가 다 끝난 후에, 한 번도 나누어떨어진 적이 없다면 소수!
         if (isPrime) {
             pls++;
@@ -31,4 +29,5 @@ int main() {
 
     cout << pls << endl;
     return 0;
+
 }

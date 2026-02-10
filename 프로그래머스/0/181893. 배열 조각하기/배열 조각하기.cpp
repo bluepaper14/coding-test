fecb66c9055dfa0793b1ea811d9bee4439842fa1
공지현 <83187050+bluepaper14@users.noreply.github.com>
2026-02-10 22:10:01 +0900

    [level 0] Title: 배열 조각하기, Time: 1.71 ms, Memory: 6.66 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.cpp
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.cpp
@@ -1,23 +1,17 @@
+#include <string>
 #include <vector>
 
 using namespace std;
 
 vector<int> solution(vector<int> arr, vector<int> query) {
-    for (int i = 0; i < query.size(); i++) {
-        vector<int> temp;
-
-        if (i % 2 == 0) { //짝수라면
-            for (int j = 0; j <= query[i]; j++) {
-                temp.push_back(arr[j]);
-            }
-        } 
-        else {
-            for (int j = query[i]; j < arr.size(); j++) {
-                temp.push_back(arr[j]);
-            }
+    vector<int> answer;
+    for(int i=0;i<query.size();i++){
+        if(i % 2 == 0){ //값이 짝수일때(x) ->  인덱스의 서순?짝수일때
+            arr.erase(arr.begin() + query[i] + 1, arr.end()); //query[i] 이후에 대한 배열 삭제.
+            //arr.begin() => 0번인데 query[i]만큼 뒤로 이동..1을 더해 내 뒤사람으로 이동했음. 
+        }else{
+            arr.erase(arr.begin(), arr.begin() + query[i]);
         }
-        arr = temp;
     }
-
     return arr;
 }

d2927a1d0fa8ae3e3421398b709f070305554fb2
공지현 <83187050+bluepaper14@users.noreply.github.com>
2026-02-13 17:09:39 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    int temp = gcd(a, b);
+    b /= temp;
+    
+    while (b % 2 == 0) b /= 2;
+    while (b % 5 == 0) b /= 5;
+    
+    return (b == 1) ? 1 : 2;
+    
+    return answer;
+}

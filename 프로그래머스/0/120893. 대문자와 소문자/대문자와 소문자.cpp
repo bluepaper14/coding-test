9d443adfcce48524043f170ffe6045041d414d70
공지현 <83187050+bluepaper14@users.noreply.github.com>
2026-02-20 12:50:40 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+string solution(string my_string) {
+    for(int i = 0; i < my_string.length(); i++) {
+        if (islower(my_string[i])) {
+            my_string[i] = toupper(my_string[i]);
+        } 
+        else if (isupper(my_string[i])) {
+            my_string[i] = tolower(my_string[i]);
+        }
+    }
+    return my_string;
+}

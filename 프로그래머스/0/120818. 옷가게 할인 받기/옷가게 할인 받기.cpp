4cc0663c58096a2e6b30b29cc5373dc9d222071c
공지현 <83187050+bluepaper14@users.noreply.github.com>
2026-02-20 13:12:33 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int price) {
+    int answer = 0;
+    if(price >= 500000) {
+        price = price - (price) * 0.2;
+        answer = price;
+    }
+    else if(price >= 300000) {
+        price = price - (price) * 0.1;
+        answer = price;
+    }
+    else if(price >= 100000) {
+        price = price - (price) * 0.05;
+        answer = price;
+    }
+    else {
+        answer = price;
+    }
+    
+    return answer;
+}

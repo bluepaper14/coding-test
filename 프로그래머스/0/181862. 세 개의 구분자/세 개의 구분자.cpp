a5c90779ea52dee8e83f7ec66e42b8c06bb57336
공지현 <83187050+bluepaper14@users.noreply.github.com>
2026-03-06 21:35:41 +0900

    [level 0] Title: 세 개의 구분자, Time: 39.82 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string myStr) {
+    vector<string> answer;
+    string temp;
+    for(int i = 0; i < myStr.length(); i++) {
+        if(myStr[i] == 'a' || myStr[i] == 'b' || myStr[i] == 'c') {
+            if(!temp.empty()) {
+                answer.push_back(temp);
+                temp = "";
+            }
+        }
+        else
+            temp += myStr[i];
+    }
+    if (!temp.empty()) {
+        answer.push_back(temp);
+    }
+    if (answer.empty()) {
+        answer.push_back("EMPTY");
+    }
+    
+    return answer;
+}

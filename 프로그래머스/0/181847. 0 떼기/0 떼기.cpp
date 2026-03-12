e40d4b8fda147cb142222e2d53f214a55377ab3d
공지현 <83187050+bluepaper14@users.noreply.github.com>
2026-03-12 16:10:23 +0900

    [level 0] Title: 0 떼기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string n_str) {
+    string answer = "";
+    for(int i = 0; i < n_str.length(); i++) {
+        if(n_str[i] != '0') {
+            for(int j = i; j < n_str.length(); j++) {
+                answer += n_str[j];
+            }
+            break;
+        }
+        
+    }
+    return answer;
+}

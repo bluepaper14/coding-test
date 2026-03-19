81aeba95a806a3e84a31441e74b30f64f5067be2
공지현 <83187050+bluepaper14@users.noreply.github.com>
2026-03-19 10:44:52 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int A = 0;
+    int B = 0;
+
+    cin >> A >> B;
+
+    string x = to_string(A);
+    string y = to_string(B);
+    
+    reverse(x.begin(), x.end());
+    reverse(y.begin(), y.end());
+
+    A = stoi(x);
+    B = stoi(y);
+
+    if(A > B) {
+        cout << A;
+    }
+    else {
+        cout << B;
+    }
+    return 0;
+}
+
+
+/*
+1.두수를 먼저 문자열로 받는다.
+2.마지막 문자열로부터 뽑아서 다시 넣는다.
+3.이를 비교한다.
+*/
+

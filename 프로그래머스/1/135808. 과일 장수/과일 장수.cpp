97c00860d5bd97a23f3168be78c96a0699e025e1
공지현 <83187050+bluepaper14@users.noreply.github.com>
2026-04-13 16:28:30 +0900

    [level 1] Title: 과일 장수, Time: 32.65 ms, Memory: 34.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    sort(score.rbegin(), score.rend());
+    
+    int val = score.size() / m; //몇 묶음인지 파악?
+    vector<int> temp; //임시벡터 생성
+    for(int i = m - 1; i < score.size(); i += m) {
+        answer += (score[i] * m);
+    }
+    return answer;
+}
+
+/*
+1점 ~ k점 사과 점수
+m개씩 담아 한상자. 
+상자에 담긴 사과중 가장 낮은 사과 점수를 p라고 가정.
+상자의 가격은 p * m
+
+많은 사과를 팔았을때 최대 이익은?
+
+
+1.먼저 핵심은 최대 이익의 가격을 내기 위해서는 정렬을 사용해 점수가 높은 사과끼리 묶을 필요가 있다.
+2.그리고 묶의 개수를 알고 있으니 이를 사용해서 임시 벡터 배열을 만들고 더하면 된다.
+*/
